bcba77c1461dd8744bbb459b081f7cf90dc58666
Julio Machado <juliocarlos00@hotmail.com>
2021-03-25 20:11:13 -0300

    add/ stripe checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,30 @@
+import { useSession, signIn } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+    const [session] = useSession();
+
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github');
+            return
+        }
+
+        
+
+    }
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button
+            type="button"
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}
+        >
             Subscribe Now
         </button>
     )
