86916d1e7370a7a81dc6442684f96c4802801510
Julio Machado <juliocarlos00@hotmail.com>
2021-04-02 17:32:20 -0300

    add/ subscription validate + redirect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,18 @@ import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
 import { ActiveLink } from '../ActiveLink';
+import Link from 'next/link';
 
 export function Header() {
 
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
+                <Link href="/">
+                    <a>
+                        <img src="/images/logo.svg" alt="ig.news" />
+                    </a>
+                </Link>
                 <nav>
                     <ActiveLink activeClassName={styles.active} href="/" >
                         <a>Home</a>
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -44,9 +44,16 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
     const { slug } = params;
 
-    // if(!session){
+    console.log(session)
 
-    // }
+    if(!session.activeSubscription){
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
 
     const prismic = getPrismicClient(req);
 
