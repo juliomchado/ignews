1829a17f39b20ff17e5950987633f01814993716
Julio Machado <juliocarlos00@hotmail.com>
2021-04-02 21:44:10 -0300

    add/ post preview page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -46,7 +46,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
     console.log(session)
 
-    if(!session.activeSubscription){
+    if(!session?.activeSubscription){
         return {
             redirect: {
                 destination: '/',
